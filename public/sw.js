3465b5e22cfdb1a4b47c240c5f20c0d0f25d21d6
HOUNZA Théophile <grandeurdivine@gmail.com>
2026-05-20 16:24:05 +0100

    bump: SW v24

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "marcheduroi-v23";
+const CACHE_NAME = "marcheduroi-v24";
 const STATIC_ASSETS = ["/", "/index.html"];
 self.addEventListener("install", (event) => {
   self.skipWaiting();
