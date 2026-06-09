1a98c1aa47c2a42589364ca2876f06e445df775c
HOUNZA Théophile <grandeurdivine@gmail.com>
2026-06-09 23:37:01 +0100

    fix vercel : routes /vitrine/slug/modifier et /payer avant SEO

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "marcheduroi-v35";
+const CACHE_NAME = "marcheduroi-v36";
 const STATIC_ASSETS = ["/", "/index.html"];
 self.addEventListener("install", (event) => {
   self.skipWaiting();
